5c30a1545d8b9f00baefa5d5402f556a649df5d2
Ipsita Routray <ipsita.routray@comviva.com>
2024-01-05 14:00:29 +0530

    changed mail id

--- src/test/java/mail_test/Q2_mail_validate_test.java
+++ src/test/java/mail_test/Q2_mail_validate_test.java
@@ -12,44 +12,24 @@ public class Q2_mail_validate_test {
 	 
     @Test
     public void testIsValidEmail() {
-        assertTrue(emailValidator.isValidEmail("user@example.com"));
-        assertFalse(emailValidator.isValidEmail("u  ser@example.com"));
+        assertTrue(emailValidator.isValidEmail("ipsita@example.com"));
+        assertFalse(emailValidator.isValidEmail("ipsi  ta@example.com"));
         assertFalse(emailValidator.isValidEmail("invalid_email"));
         assertFalse(emailValidator.isValidEmail(null));
     }
  
     @Test
     public void testIsCorporateEmail() {
-    	assertTrue(emailValidator.isValidEmail("employee@company.com"));
-        assertFalse(emailValidator.isCorporateEmail("user@example.com"));
-        assertTrue(emailValidator.isCorporateEmail("employee@company.com"));
+    	assertTrue(emailValidator.isValidEmail("ipsita@company.com"));
+        assertFalse(emailValidator.isCorporateEmail("ipsita@example.com"));
+        assertTrue(emailValidator.isCorporateEmail("ipsita@company.com"));
         assertFalse(emailValidator.isCorporateEmail(null));
     }
- 
-//    @Test
-//    void testIsValidEmailWithAssertions() {
-//        // Using assertAll to group multiple assertions in a single test
-//        assertAll("isValidEmail",
-//                () -> assertTrue(emailValidator.isValidEmail("user@example.com")),
-//                () -> assertFalse(emailValidator.isValidEmail("invalid_email")),
-//                () -> assertFalse(emailValidator.isValidEmail(null))
-//        );
-//    }
-// 
-//    @Test
-//    void testIsCorporateEmailWithAssertions() {
-//        // Using assertAll to group multiple assertions in a single test
-//        assertAll("isCorporateEmail",
-//                () -> assertFalse(emailValidator.isCorporateEmail("user@example.com")),
-//                () -> assertTrue(emailValidator.isCorporateEmail("employee@company.com")),
-//                () -> assertFalse(emailValidator.isCorporateEmail(null))
-//        );
-//    }
  
     @Test
     public void testIsValidEmailWithMessage() {
         // Using assertWithMessage to provide a custom failure message
-        assertTrue("Valid email should return true",emailValidator.isValidEmail("user@example.com"));
+        assertTrue("Valid email should return true",emailValidator.isValidEmail("ipsita@example.com"));
         assertFalse("Invalid email should return false",emailValidator.isValidEmail("invalid_email"));
         assertFalse("Null email should return false",emailValidator.isValidEmail(null));
     }
